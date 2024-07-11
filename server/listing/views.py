6c6583e9b5f646084d84b1f7efc3246f853621a9
michael <michaelngige@gmail.com>
2024-07-11 12:02:23 +0300

    listing views

--- server/listing/views.py
+++ server/listing/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .models import Product, Category
+from .serializers import ProductSerializer
+
+
+class ProductsList(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetails(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
+
+
+class ProductCreate(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
